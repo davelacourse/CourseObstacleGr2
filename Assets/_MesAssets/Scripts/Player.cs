e067de3a74c594ad985e36fe98489b43518aa63a
ProfDave <dave.lacourse@cegeptr.qc.ca>
2024-02-15 11:18:43 -0500

    1

--- Assets/_MesAssets/Scripts/Player.cs
+++ Assets/_MesAssets/Scripts/Player.cs
@@ -14,6 +14,8 @@ public class Player : MonoBehaviour
         MouvementsJoueur();
     }
 
+    
+    
     private void MouvementsJoueur()
     {
         float positionX = Input.GetAxis("Horizontal");
